c1c2efdcbc1ae549aad02c8d710a20b7c04da28c
sapslaj <saps.laj@gmail.com>
2013-12-08 14:36:39 -0500

    Cleaned up CommonFunctions.py

--- CommonFunctions.py
+++ CommonFunctions.py
@@ -1,14 +1,18 @@
+#!/usr/bin/python
 # A Class of Methods For Common Functions
 #
 # NOTE: Code Has Not Yet Been Parsed For Errors
 #
+
 # Method Overloading, First Method Will Allow Custom Delay (ms)
 def takePicture(delay):
-    # Return Statement Will Allow Return of Exit Code
-    return system("raspistill -f -o currentpicture.png -t " + str(delay))
+	# Return Statement Will Allow Return of Exit Code
+	return system("raspistill -f -o currentpicture.png -t " + str(delay))
+
 # Convenience Method, Takes Picture With Default Delay (5s)
 def takePicture():
-    return takePicture(5000)
-# TODO Add Printing Method (Possible lpr Use)
+	return takePicture(5000)
+
+# TODO: Add Printing Method (Possible lpr Use)
 # NOTE: lpr Tested and Works
 # -r Switch Needs Consideration, Will Make Things More Efficient, May Cause Problems On Printer Error
